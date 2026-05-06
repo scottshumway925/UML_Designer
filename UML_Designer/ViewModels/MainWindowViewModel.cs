c8191c6d8960ac5e0fde73c524c893d12a3d2ff9
scottshumway925 <shumworld925@gmail.com>
2026-05-05 19:41:15 -0600

    Adding the empty project

---
+++ UML_Designer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace UML_Designer.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ UML_Designer/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UML_Designer.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
