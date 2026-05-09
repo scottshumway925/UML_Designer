e8d45a8bf6d981940758451b13bb608f483b3d9d
scottshumway925 <shumworld925@gmail.com>
2026-05-09 14:43:25 -0600

    Created the initial UI Layout

--- UML_Designer/ViewModels/MainWindowViewModel.cs
+++ UML_Designer/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,7 @@
 ﻿namespace UML_Designer.ViewModels
 {
-    public partial class MainWindowViewModel : ViewModelBase
-    {
-        public string Greeting { get; } = "Welcome to Avalonia!";
-    }
+   public partial class MainWindowViewModel : ViewModelBase
+   {
+      public string Greeting { get; } = "Welcome to Avalonia!";
+   }
 }
--- UML_Designer/Views/MainWindow.axaml.cs
+++ UML_Designer/Views/MainWindow.axaml.cs
@@ -1,12 +1,33 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace UML_Designer.Views
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void OnSelectClicked(object sender, RoutedEventArgs e)
+      {
+         System.Diagnostics.Debug.WriteLine("Select Clicked");
+      }
+
+      private void OnCreateClassNodeClicked(object sender, RoutedEventArgs e)
+      {
+         System.Diagnostics.Debug.WriteLine("Create Class Node Clicked");
+      }
+
+      private void OnCreateInterfaceNodeClicked(object sender, RoutedEventArgs e)
+      {
+         System.Diagnostics.Debug.WriteLine("Create Interface Clicked");
+      }
+
+      private void OnConnectClicked(object sender, RoutedEventArgs e)
+      {
+         System.Diagnostics.Debug.WriteLine("Connect Clicked");
+      }
+   }
 }
